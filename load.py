5b139f05b5db5b6142aef973ec98f0813ce743a4
SyedMuhammadHamza <syedmuhammadhamza.smh@gmail.com>
2021-03-05 18:21:45 +0500

    First commit

---
+++ load.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  9 00:03:01 2020
+
+@author: Syed Muhammad Hamza
+"""
+from keras.models import model_from_json
+import tensorflow as tf
+def init():
+  json_file = open('model.json','r')
+  loaded_model_json = json_file.read()
+  json_file.close()
+  loaded_model = model_from_json(loaded_model_json)
+  #load weights into new model
+  loaded_model.load_weights("model.h5")
+  print("Loaded Model from disk")
+  #compile and evaluate loaded model
+  loaded_model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+  graph = tf.get_default_graph()
+  return loaded_model,graph
